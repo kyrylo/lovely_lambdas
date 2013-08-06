3e2dc3eaebc17d613d4ec7142a13206f55bf8cdd
Kyrylo Silin <kyrylosilin@gmail.com>
2013-08-06 12:27:49 +0300

    interceptor,sequence: write better YARD docs

--- lib/lovely_lambdas/create_interceptor.rb
+++ lib/lovely_lambdas/create_interceptor.rb
@@ -2,34 +2,46 @@ module LL
 
   module_function
 
-  # Takes two Procs and wraps them in another one, composing everything into a
-  # third one that is returned. All input is piped to the +newer+ Proc. The
-  # +orig+ Proc is executed only if the +newer+ returns a positive value.
+  # @overload create_interceptor(orig, newer)
+  #   Takes two Procs and wraps them in another one, composing everything into a
+  #   third one that is returned. All input is piped to the +newer+ Proc. The
+  #   +orig+ Proc is executed only if the +newer+ returns a positive value.
   #
-  # @example
-  #   original = proc { |arg| puts "[original] The #{arg} looks fine!" }
-  #   newer = proc { |arg|
-  #     puts "[newer] Hm, let me check that argument..."
-  #     if arg == 42
-  #       true
-  #     else
-  #       puts "[newer] No, it's not 42!"
-  #     end
-  #   }
+  #   @example
+  #     original = proc { |arg| puts "[original] The #{arg} looks fine!" }
+  #     newer = proc { |arg|
+  #       puts "[newer] Hm, let me check that argument..."
+  #       if arg == 42
+  #         true
+  #       else
+  #         puts "[newer] No, it's not 42!"
+  #       end
+  #     }
   #
-  #   interceptor = LL.create_interceptor(original, newer)
-  #   interceptor.call(1)  #=> [newer] Hm, let me check that argument...
-  #                        #   [newer] No, it's not 42!
+  #     interceptor = LL.create_interceptor(original, newer)
+  #     interceptor.call(1)  #=> [newer] Hm, let me check that argument...
+  #                          #   [newer] No, it's not 42!
   #
-  #   interceptor.call(42) #=> [newer] Hm, let me check that argument...
-  #                        #   [original] The 42 looks fine!
-  # @param [Proc] orig the Proc to be executed when +newer+ returned a positive
-  #   value
-  # @param [Proc, nil] newer (nil) the Proc, which prevents/allows +orig+ to be
-  #   executed
-  # @param [Object] retval (nil) the return value for the +newer+ Proc
-  # @return [Object, Proc] the wrapped Proc or an +orig+ if +newer+ is not
-  #   specified
+  #     interceptor.call(42) #=> [newer] Hm, let me check that argument...
+  #                          #   [original] The 42 looks fine!
+  #   @param [Proc] orig the Proc to be executed when +newer+ returned a positive
+  #     value
+  #   @param [Proc, nil] newer (nil) the Proc, which prevents/allows +orig+ to be
+  #     executed
+  #   @return [Proc] the wrapped Proc
+  #
+  # @overload create_interceptor(orig)
+  #   @param [Object] orig any valid Ruby object
+  #   @return [Object] +orig+
+  #
+  # @overload create_interceptor(orig, newer, retval)
+  #   Does everything the same as `create_interceptor(orig, newer)`, but returns
+  #   the provided +retval+ object if the return value of +newer+ is falsy
+  #   @param [Proc] orig
+  #   @param [Proc] newer
+  #   @param [Object] retval (nil) the return value for the +newer+ Proc
+  #   @return [Object] the wrapped Proc or an +orig+ if +newer+ is not
+  #     specified
   def create_interceptor(orig, newer = nil, retval = nil)
     if (newer.nil?) || (!newer.is_a?(Proc) && !newer.lambda?)
       orig
--- lib/lovely_lambdas/create_sequence.rb
+++ lib/lovely_lambdas/create_sequence.rb
@@ -2,20 +2,24 @@ module LL
 
   module_function
 
-  # Consequently executes +orig+ and +newer+ respectively. The return value is
-  # a new Proc, whose return value is the return value of +orig+.
+  # @overload create_sequence(orig, newer)
+  #   Consequently executes +orig+ and +newer+ respectively. The return value is
+  #   a new Proc, whose return value is the return value of +orig+.
   #
-  # @example
-  #   first = ->(noun) { puts "Have you passed through this #{noun}?"; 'yes' }
-  #   second = ->(noun) { puts "The #{noun} is beautiful, isn't it?" }
-  #   sequence = LL.create_sequence(first, second)
-  #   have_you = sequence.call('night') #=> Have you passed through this night?
-  #                                     #   The night is beautiful, isn't it?
-  #   puts have_you #=> yes
-  # @param [Proc] orig the first proc to be executed
-  # @param [Proc, nil] newer (nil) the second proc to be executed
-  # @return [Object, Proc] the wrapped Proc or an +orig+ if the +newer+ is not
-  #   specified
+  #   @example
+  #     first = ->(noun) { puts "Have you passed through this #{noun}?"; 'yes' }
+  #     second = ->(noun) { puts "The #{noun} is beautiful, isn't it?" }
+  #     sequence = LL.create_sequence(first, second)
+  #     have_you = sequence.call('night') #=> Have you passed through this night?
+  #                                       #   The night is beautiful, isn't it?
+  #     puts have_you #=> yes
+  #   @param [Proc] orig the first proc to be executed
+  #   @param [Proc] newer (nil) the second proc to be executed
+  #   @return [Proc] the wrapped Proc
+  #
+  # @overload create_sequence(orig)
+  #   @param [Object] orig
+  #   @return [Object] +orig+
   def create_sequence(orig, newer = nil)
     if newer.nil?
       orig
